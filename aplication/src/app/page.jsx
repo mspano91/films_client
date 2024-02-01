db0d064d1a6d9b2dabd666dc0ec631259cc9a4d7
mspano <mspanopablo@gmail.com>
2024-01-31 22:48:42 -0300

    first steps with next confi

---
+++ aplication/src/app/page.jsx
@@ -0,0 +1,6 @@
+import HomePage from "@/components/homePage/HomePage";
+
+async function Home() {
+  return <HomePage />;
+}
+export default Home;
---
+++ aplication/src/components/homePage/HomePage.jsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1> este va ser el main puta madre</h1>
+    </div>
+  );
+};
+
+export default HomePage;
